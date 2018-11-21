a2d8c7d321f69cb5c42552303a714fb8e9eef2b0
Fabián Jiménez <fjimenez1999@hotmail.com>
2018-11-20 18:27:39 -0600

    agregamos randoms

--- Neurona.h
+++ Neurona.h
@@ -1,10 +1,20 @@
-#pragma once //qt estaba tirando un error al usar include guards normales en archivos especificos, por eso pragma once
+#pragma once
 
 #include "conexion.h"
 #include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <QRandomGenerator>
+#include <QTime>
+
+class Conexion;
+
+
 class Neurona{
-    float carga;
-    float umbral;
+    QRandomGenerator *random1 =  new QRandomGenerator((uint) QTime::currentTime().msec());
+    QRandomGenerator *random2 =  new QRandomGenerator((uint) QTime::currentTime().second());
+    float carga = random1->bounded(1.00);
+    float umbral = random2->bounded(1.00);
     Lista<Conexion> *conexiones;
 public:
     int AgregarConexion(Neurona *neurona, float peso) ;
--- conexion.h
+++ conexion.h
@@ -1,10 +1,15 @@
-#pragma once //qt estaba tirando un error al usar include guards normales en archivos especificos, por eso pragma once
+
+#pragma once  //qt estaba tirando un error al usar include guards normales en archivos especificos, por eso pragma once
 #include "neurona.h"
+#include <QRandomGenerator>
+#include <QTime>
+class Neurona;
 class Conexion {
+    QRandomGenerator *random1 =  new QRandomGenerator((uint) QTime::currentTime().msec());
     Neurona *r ;
-    float peso ;
+    float peso = random1->bounded(1.00);
 public:
-    Conexion(Neurona *neurona, float peso){
+    Conexion(Neurona *neurona=nullptr, float peso=0){
         r=neurona;
         this->peso=peso;
     }
